ed9a17f32feb2299d63b9db2d3d166cfaea3902e
VNiemi <ville.niemi@sykemediapaja.com>
2017-10-10 20:31:58 +0300

    Assignment two, I hope. It works on my system, but since I have a different version of Unity AND had to change Text Mesh PRO version to compatible one, the commit had to be staged selectively and I might have made an error there.

---
+++ Space Shooter/Assets/Code/PlayerSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+	public class PlayerSpawn : MonoBehaviour
+	{
+
+		[SerializeField]
+		private GameObject _prefabToSpawn;
+
+		private PlayerSpaceShip _spawnedPlayer;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+			// Spawn the original player ship.
+			Spawn ();
+					
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+
+		public PlayerSpaceShip Spawn ()
+		{
+			
+				GameObject spawnedObject = Instantiate (_prefabToSpawn, transform.position, transform.rotation);
+				PlayerSpaceShip _spawnedPlayer = spawnedObject.GetComponent<PlayerSpaceShip> ();
+				_spawnedPlayer.Spawner = this;
+				
+			return _spawnedPlayer;
+		}
+	}
+}
+
